e752c522a81425c3e6764b95164f26bbc580bed7
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-03 02:33:48 +0300

    test

--- main.c
+++ main.c
@@ -9,15 +9,16 @@
 #include <linux/blk-mq.h>
 
 static int __init my_ramdisk_init(void){
-    pr_info("Disk activate")
+    pr_info("Disk activate");
     return 0;
 }
 
 static int __exit my_ramdisk_exit(void){
-    pr_info("Disk deactivate")
+    pr_info("Disk deactivate");
+    return 0;
 }
 
 module_init(my_ramdisk_init);
 module_exit(my_ramdisk_exit);
 
-MODULE_LICENSE("GPL-2.0")
+MODULE_LICENSE("GPL-2.0");
