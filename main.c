d3cfb7d5f735903f81256527332dcfe0866d1f9a
vladimirmitnovitskiy <harrellprmime@gmail.com>
2026-05-06 16:47:49 +0300

    test blkdev

--- main.c
+++ main.c
@@ -8,16 +8,50 @@
 #include <linux/blkdev.h>
 #include <linux/blk-mq.h>
 
+#define DISK_SIZE (50 * 1024 * 1024)
+#define DISK_NAME "myramdisk"
+
+struct my_ramdisk {
+    int size;
+    u8 *data;
+};
+
+static struct my_ramdisk *device = NULL;
+static int major_num = 0;
+
+
+
 static int __init my_ramdisk_init(void){
-    pr_info("Disk activate\n");
+    device = kzalloc(sizeof(my_ramdisk), GFP_KERNEL);
+    if (!device) return -ENOMEM;
+
+    device->size = DISK_SIZE;
+    device->data = vmalloc(device->size);
+    if (!device->data){
+        kfree(device);
+        return -ENOMEM;
+    }
+
+    major_num = register_blkdev(0, DISK_NAME);
+    if (major_num < 0){
+        vfree(device->data);
+        kfree(device);
+        return major_num;
+    }
+
+    pr_info("Disk activate! Выделено 50 МБ памяти. Можорный номер: %d\n", major_num);
     return 0;
 }
 
 static void __exit my_ramdisk_exit(void){
+    unregister_blkdev(major_num, DISK_NAME);
+    vfree(device->data);
+    kfree(device);
+
     pr_info("Disk deactivate\n");
 }
 
 module_init(my_ramdisk_init);
 module_exit(my_ramdisk_exit);
 
-MODULE_LICENSE("GPL-2.0");
+MODULE_LICENSE("GPL");
